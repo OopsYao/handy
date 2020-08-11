3b76f7856b6ed1a21cfd865bd91a72b68a8107c3
oopsy <whoops.yao@gmail.com>
2020-08-11 18:39:57 +0800

    🎨 Add code format spec

--- src/pages/textbreaker.js
+++ src/pages/textbreaker.js
@@ -1,43 +1,37 @@
-import React from 'react';
-// import breaker from '../textbreaker';
-import {
-    readText,
-    // writeText,
-} from '../utils';
+import React from 'react'
+import { readText } from '../utils'
 
-const base64 = blob => {
-    const reader = new FileReader();
-    reader.readAsDataURL(blob);
-    return new Promise(resolve => {
-        reader.onload = () => {
-            resolve(reader.result);
-        };
-    });
+const base64 = (blob) => {
+  const reader = new FileReader()
+  reader.readAsDataURL(blob)
+  return new Promise((resolve) => {
+    reader.onload = () => {
+      resolve(reader.result)
+    }
+  })
 }
 
-const handle = _ => {
-    (async () => {
-        try {
-            const text = await readText();
-            console.log(text);
-            for (const item of text) {
-                if (item.types !== "image/png") {
-                    alert("Clipboard contains non-image data. Unable to access it.");
-                } else {
-                    const blob = await item.getType("image/png");
-                    const imgBase64 = await base64(blob);
-                    console.log(imgBase64);
-                }
-            }
-            // const chunklist = breaker(text, 20);
-            // writeText(chunklist.join('\n'));
-        } catch {
-
+const handle = (_) => {
+  ;(async () => {
+    try {
+      const text = await readText()
+      console.log(text)
+      for (const item of text) {
+        if (item.types !== 'image/png') {
+          alert('Clipboard contains non-image data. Unable to access it.')
+        } else {
+          const blob = await item.getType('image/png')
+          const imgBase64 = await base64(blob)
+          console.log(imgBase64)
         }
-    })();
+      }
+      // const chunklist = breaker(text, 20);
+      // writeText(chunklist.join('\n'));
+    } catch {}
+  })()
 }
 export default () => (
-    <div>
-        <button onClick={handle}>CLICK</button>
-    </div>
+  <div>
+    <button onClick={handle}>CLICK</button>
+  </div>
 )
