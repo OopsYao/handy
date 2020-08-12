dd8d8268de0a6321a9210514aaba351d7478008b
oopsy <whoops.yao@gmail.com>
2020-08-12 15:02:52 +0800

    🚚 Rename files

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import * as serviceWorker from './serviceWorker'
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Textbreaker from './pages/textbreaker';
+import App from './App'
+import Flow from './pages/Flow'
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Switch>
-        <Route path='/textbreaker' component={Textbreaker} />
-        <Route path='/' component={App} />
+        <Route path="/flow" component={Flow} />
+        <Route path="/" component={App} />
       </Switch>
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById('root'),
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
