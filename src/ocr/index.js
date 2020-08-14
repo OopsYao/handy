ce27da93846dbfad59fbfe2250de70a3efb6f24c
oopsy <whoops.yao@gmail.com>
2020-08-14 20:56:06 +0800

    🔒 Remove hard-coded token

--- src/ocr/index.js
+++ src/ocr/index.js
@@ -1,9 +1,8 @@
 import { ocr as AipOcrClient } from 'baidu-aip-sdk'
 
-const APP_ID = process.env.BAIDU_AI_APP_ID || '21936987'
-const API_KEY = process.env.BAIDU_AI_API_KEY || 'oeS2vicz0g2BnEZ8OEuaDhA3'
-const SECRET_KEY =
-  process.env.BAIDU_AI_SECRET_KEY || 'yTvX2FSvUSfhn8XdQ89Q0VGzgsmaAOND'
+const APP_ID = process.env.BAIDU_AI_APP_ID
+const API_KEY = process.env.BAIDU_AI_API_KEY
+const SECRET_KEY = process.env.BAIDU_AI_SECRET_KEY
 
 const client = new AipOcrClient(APP_ID, API_KEY, SECRET_KEY)
 
